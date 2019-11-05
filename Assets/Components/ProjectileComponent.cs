5c5ae3d0af94072eac4ba2811bbb04e897d451da
oberdm <oberdm@plu.edu>
2019-11-04 19:25:36 -0800

    Added 3 components
    ProjectileComponent, RotationComponent, XformComponent

---
+++ Assets/Components/ProjectileComponent.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public struct ProjectileComponent : IComponentData
+{
+    public string projectileName; // ID of the projectile
+}
---
+++ Assets/Components/XformComp.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public struct XformComponent : IComponentData
+{
+    public boolean hasXform; // transform flag
+}
