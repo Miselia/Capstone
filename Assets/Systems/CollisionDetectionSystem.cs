3ec4a4e908cb6deff32a8570a4d9b4eb0b6bd234
Keith Petitt <keaver021@gmail.com>
2019-11-11 10:37:13 -0800

    Initial Collision System commits

---
+++ Assets/Systems/CollisionDetectionSystem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class CollisionDetectionSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref XformComponent xform, ref CollisionComponent collComp) =>
+        {
+
+        });
+    }
+}
