7d50848a2a541b2d0709d4debe21f74c27708740
Keith Petitt <keaver021@gmail.com>
2019-11-17 14:02:09 -0800

    Fixed Merge Issues, Now Rendering

--- Assets/Systems/TestingSystem.cs
+++ Assets/Systems/TestingSystem.cs
@@ -5,15 +5,10 @@ using Unity.Entities;
 
 public class TestingSystem : ComponentSystem
 {
-    
-
-    
     protected override void OnUpdate() {
         Entities.ForEach((ref TestingComponent testingComponent, ref MovementComponent movementComponent) => {
             testingComponent.testingValue += 1f;
             Debug.Log(testingComponent.testingValue);
-
-            
         });
     }
 }
