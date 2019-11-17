bf301571769521d317667a3ffc3825c0b294e06e
Keith Petitt <keaver021@gmail.com>
2019-11-17 14:29:42 -0800

    Minor changes to PlayerEntity and Game.cs

--- Assets/Entities/PlayerEntity.cs
+++ Assets/Entities/PlayerEntity.cs
@@ -7,19 +7,21 @@ using Unity.Transforms;
 
 public static class PlayerEntity 
 {
-   public static Entity Create(EntityManager em, Vector2 movementVector, Vector2 position, int playerID, int maxHealth, Mesh mesh, Material mat)
+   public static Entity Create(EntityManager em, Vector2 movementVector, Vector2 position, float radius, int playerID, int maxHealth, Mesh mesh, Material mat)
     {
         Entity entity = em.CreateEntity();
 
         em.AddComponent( entity, typeof(XformComponent) );
         em.AddComponent( entity, typeof(MovementComponent) );
         em.AddComponent( entity, typeof(PlayerComponent) );
+        em.AddComponent(entity, typeof(CollisionComponent));
         em.AddComponent(entity, typeof(RenderMesh));
         em.AddComponent(entity, typeof(LocalToWorld));
         em.AddComponent(entity, typeof(Translation));
         
         em.SetComponentData(entity, new MovementComponent(movementVector));
         em.SetComponentData(entity, new XformComponent(position));
+        em.SetComponentData(entity, new CollisionComponent(radius));
         em.SetComponentData(entity, new PlayerComponent(playerID, maxHealth));
         em.SetSharedComponentData(entity, new RenderMesh {mesh = mesh, material = mat});
 
