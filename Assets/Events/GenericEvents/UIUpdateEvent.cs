3123b5fb053c5a319c8a02ea47cce2215061f12d
Devin <oberdm@plu.edu>
2020-01-19 12:36:16 -0800

    UIManager Implementation
    UIManager Added

---
+++ Assets/Events/GenericEvents/UIUpdateEvent.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class UIUpdateEvent : IGenericEvent
+{
+    public int p1Health;
+    public int p2Health;
+    public int p1Mana;
+    public int p2Mana;
+
+    public UIUpdateEvent(int p1Health, int p2Health, int p1Mana, int p2Mana)
+    {
+        this.p1Health = p1Health;
+        this.p2Health = p2Health;
+        this.p1Mana = p1Mana;
+        this.p2Mana = p2Mana;
+
+        Debug.Log("UI Updated");
+    }
+}
---
+++ Assets/MonoScript/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour, IGenericEventListener
+{
+    [SerializeField] private Text player1Health;
+    [SerializeField] private Text player1Mana;
+    [SerializeField] private Text player2Health;
+    [SerializeField] private Text player2Mana;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.instance.RegisterListener<UIUpdateEvent>(this);
+    }
+
+    // Update is called once per frame
+    public bool HandleEvent(IGenericEvent evt)
+    {
+        if (evt is UIUpdateEvent)
+        {
+            UIUpdateEvent ue = (UIUpdateEvent) evt;
+            player1Health.text = ("Health: " + ue.p1Health.ToString());
+            player2Health.text = ("Health: " + ue.p2Health.ToString());
+            player1Mana.text = ("Mana: " + ue.p1Mana.ToString());
+            player2Mana.text = ("Mana: " + ue.p2Mana.ToString());
+
+            return true;
+        }
+        return false;
+    }
+}
