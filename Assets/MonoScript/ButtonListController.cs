e15f7df142cc96cab66887dfe1b1ca5a883dc37e
Keith <keaver021@gmail.com>
2020-02-21 15:09:41 -0800

    Working towards Card Adding to Hand on Button Click

--- Assets/MonoScript/ButtonListController.cs
+++ Assets/MonoScript/ButtonListController.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class ButtonListController : MonoBehaviour
 {
     [SerializeField] private GameObject buttonPrefab;
+    CardLibrary cl;
     void Start()
     {
-        CardLibrary cl = (CardLibrary)GameObject.Find("CardLibrary").GetComponent("CardLibrary");
+        cl = (CardLibrary)GameObject.Find("CardLibrary").GetComponent("CardLibrary");
 
         foreach(CardData data in cl.GetListByName(0))
         {
             GameObject button = Instantiate(buttonPrefab) as GameObject;
             button.SetActive(true);
 
-            button.GetComponent<ButtonPrefab>().SetText(data.getName());
+            button.GetComponent<CardButtonPrefab>().Initialize(data.cardName, data.cardID);
             button.transform.SetParent(buttonPrefab.transform.parent, false);
         }
+    }
+
+    public void ButtonClicked(int id)
+    {
+        
     }
 }
