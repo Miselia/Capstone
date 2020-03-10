88d5d12e8d9e75ed50e03f04047232ebf7616969
oberdm <oberdm@plu.edu>
2020-03-10 13:50:37 -0700

    Jobs implemented into movement

--- Assets/Systems/MovementSystem.cs
+++ Assets/Systems/MovementSystem.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 using Unity.Entities;
 using Unity.Transforms;
 using UnityEngine.SceneManagement;
+using Unity.Jobs;
+using Unity.Burst;
 
-public class MovementSystem : ComponentSystem
+public class MovementSystem : JobComponentSystem
 {
+    /*
     protected override void OnUpdate()
     {
        
@@ -16,5 +19,25 @@ public class MovementSystem : ComponentSystem
                 translation.Value.x += moveComp.movementVector.x * Time.deltaTime;
             });
         
+    }
+    */
+    [BurstCompile]
+    private struct Job: IJobForEachWithEntity<MovementComponent, Translation>
+    {
+        public float deltaTime;
+
+        public void Execute(Entity entity, int index,ref MovementComponent moveComp, ref Translation translation)
+        {
+            translation.Value.y += moveComp.movementVector.y * deltaTime;
+            translation.Value.x += moveComp.movementVector.x * deltaTime;
+        }
+    }
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        Job job = new Job
+        {
+            deltaTime = Time.deltaTime,
+        };
+        return job.Schedule(this, inputDeps);
     }
 }
