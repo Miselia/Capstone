fb4c123e782c5fa0588394a635385c281378e5ea
Keith Petitt <keaver021@gmail.com>
2020-05-02 14:17:40 -0700

    Updated Rest of Cards and Confirmed Mana Costs in CardLibrary

--- Assets/MonoScript/CardLibrary.cs
+++ Assets/MonoScript/CardLibrary.cs
@@ -22,15 +22,15 @@ public class CardLibrary: MonoBehaviour
         library["Default"].Add(new CardData(0, "Default", 0, 0, "None", "None", cardMaterialLibrary[0]));
         library["Horror"].Add(new CardData(1, "Blood Boil", 2, 3, "Speed+", "You just make daddy so angry", cardMaterialLibrary[1]));
         library["Fantasy"].Add(new CardData(2, "Fire Bolt", 3, 1, "", "3rd Level Spell", cardMaterialLibrary[2]));
-        library["Sci-Fi"].Add(new CardData(3, "Plasma Bolt", 4, 4, "", "3rd Level Plasma", cardMaterialLibrary[3]));
+        library["Sci-Fi"].Add(new CardData(3, "Plasma Bolt", 2, 4, "", "3rd Level Plasma", cardMaterialLibrary[3]));
         library["Steampunk"].Add(new CardData(4, "Red Coins", 5, 2, "", "Collect all 8 for a star!",cardMaterialLibrary[4]));
         library["Fantasy"].Add(new CardData(5, "Glimpse Into The Ether", 5, 1, "ManaRegen+","The Ether Glimpses back", cardMaterialLibrary[5]));
         library["Fantasy"].Add(new CardData(6, "Wandering Woodsprite", 4, 1, "Healing", "A helpful friend to all", cardMaterialLibrary[6]));
         library["Fantasy"].Add(new CardData(7, "Akashic Records", 7, 1, "Random Cards", "Randomness in all things", cardMaterialLibrary[7]));
         library["Fantasy"].Add(new CardData(8, "Well of Knowledge", 3, 1, "Next Heal/Damage doubled", "Knowledge is power", cardMaterialLibrary[8]));
         library["Fantasy"].Add(new CardData(9, "Magic Record", 4, 1, "Double cast next card", "I'll save this spell for later", cardMaterialLibrary[9]));
-        library["Sci-Fi"].Add(new CardData(10, "Nano Barrier", 7, 4, "Create a barrier that can't be moved through", "No, you can't pass go", cardMaterialLibrary[10]));
-        library["Sci-Fi"].Add(new CardData(11, "Gravity Well", 7, 4, "Well pulls projectiles in.", "Zucc", cardMaterialLibrary[11]));
+        library["Sci-Fi"].Add(new CardData(10, "Nano Barrier", 4, 4, "Create a barrier that can't be moved through", "No, you can't pass go", cardMaterialLibrary[10]));
+        library["Sci-Fi"].Add(new CardData(11, "Gravity Well", 10, 4, "Well pulls projectiles in.", "Zucc", cardMaterialLibrary[11]));
         library["Sci-Fi"].Add(new CardData(12, "Electromagnetic Projectiles", 6, 4, "", "That's some pretty good ping", cardMaterialLibrary[12]));
         library["Sci-Fi"].Add(new CardData(13, "Overcharge", 4, 4, "Enhances speed of all projectiles", "Gotta go fast!", cardMaterialLibrary[13]));
         library["Steampunk"].Add(new CardData(14, "Lead Rain", 6, 2, "", "Make your opponent's life a bullet-hell", cardMaterialLibrary[14]));
